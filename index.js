e1afac3f74d7fe86d0ed11876244404e5e1ae06a
AluraHub <alura.ely@gmail.com>
2022-05-16 20:31:29 -0500

    #1 Arrays Ejercicios JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+let clientes = ["Miguel", "Sandra", "Pedro", "Mau", "Raúl"];
+
+let cliente1 = ["Joel", 123456, "jla@gmail.com", 30, 54298477];
+
+let contraseña = 123456 // input de html
+console.log(clientes[4]);
+
+// login
+
+if (contraseña) === cliente1=[1]
+
+let gibson = ["Guitarra", 700, 20, "verde", "acústica"];
+let chafamex = ["Guitarra", 100, 10, "roja", "eléctrica"];
+let yamaha = ["Guitarra", 500, 30, "azul", "eléctrica"];
+
+let tienda [gibson, chafamex, yamaha]
+console.log [tienda [2][4]]
+
+for (let i=0; i<=gibson.length; i++){
+    console.log{gibson[i]};
+}
+
+//Ejercicio 3
+// crear arreglo con los tres arreglos anteriores
+// imprimir en consola
